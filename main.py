477f34a28fc108d7e6d23504158e77ebc26418a3
rezarinaldi <rez4rinaldi@gmail.com>
2025-05-22 11:20:22 +0700

    chore: day 18 done ✅, sometime getting issue

--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 from dotenv import load_dotenv
 from agents import Agent, Runner
 from agents.mcp import MCPServer, MCPServerStdio
-import asyncio
 import os
 
 load_dotenv()
@@ -11,27 +10,27 @@ load_dotenv()
 async def run(mcp_servers: list[MCPServer]):
     agent = Agent(
         name="Assistant",
-        instructions="""You are a funny person, and helpful assistant.
+        instructions="""You are a helpful assistant.
         Only use mcp_severs available if needed.
         """,
         model="gpt-4.1",
         mcp_servers=mcp_servers,
     )
 
-    result = await Runner.run(agent, input="Write a jokes about designer!")
+    result = await Runner.run(
+        agent, input="Go to indrazm.com and summarize the content!"
+    )
     print(result.final_output)
 
 
 async def main():
     async with MCPServerStdio(
-        {
-            "command": "npx",
-            "args": ["-y", "@modelcontextprotocol/server-github"],
-            "env": {"GITHUB_PERSONAL_ACCESS_TOKEN": os.getenv("GITHUB_TOKEN")},
-        }
+        {"command": "npx", "args": ["mcp-remote", "http://localhost:8000/mcp", "8080"]}
     ) as mcp_server:
         await run([mcp_server])
 
 
 if __name__ == "__main__":
+    import asyncio
+
     asyncio.run(main())
